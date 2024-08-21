46d23cb029c98545296e3a0d9d7df9c65efb6297
life_robot <life_robot@noreply.gitcode.com>
2024-08-21 17:33:15 +0800

    1. publicly disclose third-party external references to websocket

--- libs/cbsk_socket_tokio/src/lib.rs
+++ libs/cbsk_socket_tokio/src/lib.rs
@@ -2,6 +2,10 @@
 
 #[cfg(feature = "cbsk_socket")]
 pub use cbsk_socket;
+#[cfg(feature = "tokio-tungstenite")]
+pub use tokio_tungstenite;
+#[cfg(feature = "futures-util")]
+pub use futures_util;
 
 pub(crate) mod r#macro;
 #[cfg(any(feature = "tcp_server", feature = "tcp_client"))]
